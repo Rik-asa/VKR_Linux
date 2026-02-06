d2bc6914cc2c1e433e7539ffe12b0fb1d0df7c5c
Rik-asa <margo.sharafeeva@mail.ru>
2026-02-06 23:51:11 +0300

    первоначальная + оконка

--- kpi_core/settings.py
+++ kpi_core/settings.py
@@ -161,6 +161,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # REST Framework только в режиме работы
